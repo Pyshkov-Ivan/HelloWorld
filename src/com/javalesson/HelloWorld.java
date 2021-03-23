b43d14b81a0e38cb6662b3c983ae57e5a4490a84
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-23 22:45:28 +0300

    Переменные Объявление переменных. Часть 2 Char Объявление переменных и вывод

--- src/com/javalesson/HelloWorld.java
+++ src/com/javalesson/HelloWorld.java
@@ -22,6 +22,20 @@ public class HelloWorld {
 
         System.out.println("MyDoubleResult = " + myNewDouble / 3);
 
+        char char1 = 'A';
+        char char2 = 'b';
+        char char3 = '\n';
+        char char4 = '^';
+        char char5 = '\u00A9';
+
+        System.out.println("Characters");
+        System.out.println("Char1 = " + char1);
+        System.out.println("Char2 = " + char2);
+        System.out.println("Char3 = " + char3);
+        System.out.println("Char4 = " + char4);
+        System.out.println("Char5 = " + char5);
+
+
         System.out.println("b = " + b);
         System.out.println("c = " + c);
         System.out.println("a = " + a);
