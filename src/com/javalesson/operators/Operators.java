52aa0c407077e041c01de219cd6ca8ae475d3b65
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-23 23:45:15 +0300

    Операторы Часть 1 Инкрементирование Декрементирование Префексная операция Расхождение постфиксной операции с префиксной

--- src/com/javalesson/operators/Operators.java
+++ src/com/javalesson/operators/Operators.java
@@ -29,5 +29,15 @@ public class Operators {
 
         --b;
         System.out.println("New b = " + b);
+
+
+        double n = 7;
+        double m = 7;
+
+        double res1 = 2 * n++;
+        double res2 = 2 * ++m;
+
+        System.out.println("Res1 = " + res1);
+        System.out.println("Res2 = " + res2);
     }
 }
