6995f51c927e414b547be444f9e0eeec0b1ecb51
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-23 22:24:58 +0300

    Переменные Объявление переменных

--- src/com/javalesson/HelloWorld.java
+++ src/com/javalesson/HelloWorld.java
@@ -4,5 +4,19 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+
+        byte b = 10;
+        short c = 200;
+        int a = b + 23;
+        long myLongValue = 123_456_789_876L;
+
+        float myFloatVal = 23.56f;
+        double myDoubleVal = 23.56d;
+
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("a = " + a);
+        System.out.println("MtLongValue = " + myLongValue);
+
     }
 }
