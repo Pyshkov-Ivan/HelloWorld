0555d41e39e15f0cc15ec6e73637cbb36c662e52
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-24 23:04:28 +0300

    Условные операторы if - else Вложенное условие

--- src/com/javalesson/controlstatement/Controls.java
+++ src/com/javalesson/controlstatement/Controls.java
@@ -15,6 +15,10 @@ public class Controls {
         System.out.println("You've got");
         if (testScore >= 90) {
             System.out.println("an Excellent");
+        } else if (testScore >= 75) {
+            System.out.println("a Good");
+        } else if (testScore >= 60) {
+            System.out.println("a Satisfactory");
         } else {
             System.out.println("a bad");
         }
