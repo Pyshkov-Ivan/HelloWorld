060988f9262240d3f02ab1b6e3dbe5af55c7854c
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-24 22:54:18 +0300

    Условные операторы if - else

--- src/com/javalesson/controlstatement/Controls.java
+++ src/com/javalesson/controlstatement/Controls.java
@@ -6,9 +6,14 @@ public class Controls {
         int i = 10;
         {
             int k = 15;
+            System.out.println("I = " + i);
+            System.out.println("K = " + k);
         }
-        System.out.println("I = " + i);
-        System.out.println("K = " + k);
+
+
+        int testScore = 95;
+        if (testScore>90)
+            System.out.println("Excellent");
 
 
     }
