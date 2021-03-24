b69a1b733634bb4cfdb7cb1a8af41877171f1f0f
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-24 22:49:10 +0300

    Условные операторы if - else Видимость переменных и код блоки

--- src/com/javalesson/controlstatement/Controls.java
+++ src/com/javalesson/controlstatement/Controls.java
@@ -3,6 +3,12 @@ package com.javalesson.controlstatement;
 public class Controls {
     public static void main(String[] args) {
 
+        int i = 10;
+        {
+            int k = 15;
+        }
+        System.out.println("I = " + i);
+        System.out.println("K = " + k);
 
 
     }
