c51fd50f7e18b1637c630178add330e5ad654b97
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-25 23:31:02 +0300

    Циклический оператор For Факториал 10

---
+++ src/com/javalesson/controlstatement/ForLoop.java
@@ -0,0 +1,13 @@
+package com.javalesson.controlstatement;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int factorialLimit = 10;
+        int factorial = 1;
+        for (int i = 1; i <= factorialLimit; i++) {
+            factorial *= i;
+            System.out.println("Factorial = " + factorial);
+        }
+        System.out.println("Final factorial value = " + factorial);
+    }
+}
