5d7da1f8a8b131caca7b5eba3c4ecd7f20fd00f7
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-25 23:10:20 +0300

    Циклический оператор DoWhile Сколько лет потребуется чтобы накопить 100к с балансом 0 за 15 лет

--- src/com/javalesson/controlstatement/WhileDo.java
+++ src/com/javalesson/controlstatement/WhileDo.java
@@ -3,8 +3,8 @@ package com.javalesson.controlstatement;
 public class WhileDo {
     public static void main(String[] args) {
         int finalBalance = 100000;
-        double currentBalance = 80000;
-        int payment = 10000;
+        double currentBalance = 0;
+        int payment = 1000;
         int years = 0;
         double interestRate = 0.1;
 
@@ -13,6 +13,6 @@ public class WhileDo {
             currentBalance = currentBalance + currentBalance * interestRate;
             years++;
             System.out.println("Year " + years + " - " + currentBalance);
-        } while (currentBalance < finalBalance);
+        } while (currentBalance < finalBalance && years < 15);
     }
 }
