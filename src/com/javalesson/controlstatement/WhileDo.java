f27009414e8ed07029c298b0363005baf88579e8
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-03-25 23:02:12 +0300

    Циклический оператор DoWhile

--- src/com/javalesson/controlstatement/WhileDo.java
+++ src/com/javalesson/controlstatement/WhileDo.java
@@ -3,16 +3,16 @@ package com.javalesson.controlstatement;
 public class WhileDo {
     public static void main(String[] args) {
         int finalBalance = 100000;
-        double currentBalance = 0;
+        double currentBalance = 120000;
         int payment = 10000;
         int years = 0;
         double interestRate = 0.1;
 
-        while (currentBalance < finalBalance) {
+        do {
             currentBalance += payment;
             currentBalance = currentBalance + currentBalance * interestRate;
             years++;
             System.out.println("Year " + years + " - " + currentBalance);
-        }
+        } while (currentBalance < finalBalance);
     }
 }
