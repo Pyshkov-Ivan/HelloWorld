0bca1b4806f35d2eb76c3f1b0297df9339cdf642
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-04-08 15:53:01 +0300

    Тестирование пройденных уроков

---
+++ src/com/javalesson/testing/TestingLessons.java
@@ -0,0 +1,18 @@
+package com.javalesson.testing;
+
+public class TestingLessons {
+    public static void main(String[] args) {
+        int Regress = 80;
+        System.out.println("So");
+        if (Regress >= 250) {
+            System.out.println("Mission");
+            System.out.println("Complete");
+        } else if (Regress >= 125) {
+            System.out.println("Mission");
+            System.out.println("Not Finished");
+        } else {
+            System.out.println("Failed");
+        }
+
+    }
+}
