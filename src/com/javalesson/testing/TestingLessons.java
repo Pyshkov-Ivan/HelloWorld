5fe9303fb662c91c1be82d51e81a5a8bbc4df907
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-06-01 16:50:20 +0300

    Тестирование изученных уроков Ступор в изучении Повторение типов данных Далее Операторы

--- src/com/javalesson/testing/TestingLessons.java
+++ src/com/javalesson/testing/TestingLessons.java
@@ -11,6 +11,31 @@ public class TestingLessons {
         float myFloatValue = 23.56F;
         double myDoubleValue = 56.45d;
 
+        double sum = a + b + myLongValue + myDoubleValue + myFloatValue;
+        System.out.println("SUM = " + sum);
+
+        float floatResult = myFloatValue / 3;
+        System.out.println("floatResult = " + floatResult);
+        double myNewDouble = 23.56;
+
+        System.out.println("myDoubleResult = " + myNewDouble / 3);
+
+        char char1 = 'A';
+        char char2 = 'b';
+        char char3 = '\n';
+        char char4 = '^';
+        char char5 = '\u00A9';
+
+        System.out.println("Characters");
+        System.out.println("Char1 = " + char1);
+        System.out.println("Char2 = " + char2);
+        System.out.println("Char3 = " + char3);
+        System.out.println("Char4 = " + char4);
+        System.out.println("Char5 = " + char5);
+
+        System.out.println("SUM of Characters");
+        System.out.println(char1 + char2 + char3 + char4 + char5);
+
         System.out.println("byte b = " + b);
         System.out.println("short c = " + c);
         System.out.println("int a = " + a);
