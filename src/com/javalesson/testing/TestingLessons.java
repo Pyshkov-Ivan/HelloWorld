12f50b9a36106f1b96f1996b6e43fecb57201650
Pyshkov-Ivan <ivan-pyshkov@mail.ru>
2021-06-02 15:07:54 +0300

    Тестирование изученных уроков Ступор в изучении Повторение типов данных Условный оператор if else statements

--- src/com/javalesson/testing/TestingLessons.java
+++ src/com/javalesson/testing/TestingLessons.java
@@ -2,44 +2,25 @@ package com.javalesson.testing;
 
 public class TestingLessons {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        byte b = 10;
-        short c = 200;
-        int a = b + 23;
-        long myLongValue = 123_456_789_876L;
 
-        float myFloatValue = 23.56F;
-        double myDoubleValue = 56.45d;
+        int i = 10;
+        {
+            int k = 15;
+            System.out.println("I = " + i);
+            System.out.println("K = " + k);
+        }
 
-        double sum = a + b + myLongValue + myDoubleValue + myFloatValue;
-        System.out.println("SUM = " + sum);
-
-        float floatResult = myFloatValue / 3;
-        System.out.println("floatResult = " + floatResult);
-        double myNewDouble = 23.56;
-
-        System.out.println("myDoubleResult = " + myNewDouble / 3);
-
-        char char1 = 'A';
-        char char2 = 'b';
-        char char3 = '\n';
-        char char4 = '^';
-        char char5 = '\u00A9';
-
-        System.out.println("Characters");
-        System.out.println("Char1 = " + char1);
-        System.out.println("Char2 = " + char2);
-        System.out.println("Char3 = " + char3);
-        System.out.println("Char4 = " + char4);
-        System.out.println("Char5 = " + char5);
-
-        System.out.println("SUM of Characters");
-        System.out.println(char1 + char2 + char3 + char4 + char5);
-
-        System.out.println("byte b = " + b);
-        System.out.println("short c = " + c);
-        System.out.println("int a = " + a);
-
-
+        int testScore = 95;
+        System.out.println("You've got");
+        if (testScore >= 90) {
+            System.out.println("an Excellent");
+        } else if (testScore >= 75) {
+            System.out.println("a Good");
+        } else if (testScore >= 60) {
+            System.out.println("a Satisfactory");
+        } else {
+            System.out.println("an Excellent");
+        }
+        System.out.println("mark");
     }
 }
